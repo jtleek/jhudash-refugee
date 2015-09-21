1c41bcbd7d7abae6e3dbf6adf315c5327f4d34af
jtleek <jtleek@gmail.com>
2015-09-21 12:09:57 -0400

    updated with googlesheets

---
+++ read-google-sheet.R
@@ -0,0 +1,5 @@
+library(gplots)
+library(googlesheets)
+my_url = "https://docs.google.com/spreadsheets/d/1eRDlcwkT0hqqi-DNVU4dgJHxKRsLYY0VAqOMjxcD2r8/pubhtml"
+my_gs = gs_url(my_url)
+dat = gs_read(my_gs)
